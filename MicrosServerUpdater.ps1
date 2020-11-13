ca4d283a8ee41f5ad02aa007bcf5f4ec2977c603
RogueRAZR <48629324+RogueRAZR@users.noreply.github.com>
2020-11-13 02:08:56 -0700

    Added Error Checking
    Added Try/Catch for RES Shutdown, Update, and Reboot Check

--- MicrosServerUpdater.ps1
+++ MicrosServerUpdater.ps1
@@ -1,39 +1,30 @@
-@echo off
 D:
 cd D:\micros\res\pos\bin
 # Shutdown RES Systems
-clcontrol.exe /SYSTEM IDLE
-Write-Output "*********************************************************************
-*                                                                   *
-*                      SHUTTING DOWN RES3700...                     *
-*                                                                   *
-*********************************************************************"
+Try {
+	clcontrol.exe /SYSTEM IDLE
+}
+Catch {
+	Write-Output "Failed to shutdown the RES DB: " $_
+}
 C:
 cd C:\Windows\System32
-# Check for and install windows updates.
-Get-WUInstall -MicrosoftUpdate -AcceptAll
-Write-Output "*********************************************************************
-*                                                                   *
-*                         Updating Windows                          *
-*                                                                   *
-*********************************************************************"
-# Reboot only if required
-$Reboot = Get-WURebootStatus -Silent
-IF ($Reboot -eq "True") {
-	Get-WURebootStatus -AutoReboot
-	Write-Output "*********************************************************************
-*                                                                   *
-*                           REBOOTING!!!                            *
-*                                                                   *
-*********************************************************************"
+# Check for and install windows updates. Catch Exception if WinUpdate Fails
+Try {
+	Get-WUInstall -MicrosoftUpdate -AcceptAll
+	# Reboot only if required
+	$Reboot = Get-WURebootStatus -Silent
+	IF ($Reboot -eq "True") {
+		Get-WURebootStatus -AutoReboot
 	}
-	else
-	{
-	Write-Output "*********************************************************************
-*                                                                   *
-*                            COMPLETE!!!                            *
-*                                                                   *
-*********************************************************************"
-	Start-Sleep -s 15
+
+	else {
+		Write-Output "Windows Update has finshed installing updates, no reboot is required."
+		Start-Sleep -s 15
 	}
+}
+
+Catch {
+	Write-Output "Windows Update has Failed: " $_
+}
 exit
