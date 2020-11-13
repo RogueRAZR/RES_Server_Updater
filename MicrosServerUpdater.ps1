a1c0130c8b86ee7014ab93761ad5a4283c85b89a
RogueRAZR <48629324+RogueRAZR@users.noreply.github.com>
2020-11-13 02:11:45 -0700

    Update MicrosServerUpdater.ps1

--- MicrosServerUpdater.ps1
+++ MicrosServerUpdater.ps1
@@ -14,16 +14,15 @@ Try {
 	Get-WUInstall -MicrosoftUpdate -AcceptAll
 	# Reboot only if required
 	$Reboot = Get-WURebootStatus -Silent
-	IF ($Reboot -eq "True") {
+	If ($Reboot -eq "True") {
+		Write-Output "Restart Required, Rebooting in 15 seconds."
 		Get-WURebootStatus -AutoReboot
 	}
-
 	else {
 		Write-Output "Windows Update has finshed installing updates, no reboot is required."
 		Start-Sleep -s 15
 	}
 }
-
 Catch {
 	Write-Output "Windows Update has Failed: " $_
 }
